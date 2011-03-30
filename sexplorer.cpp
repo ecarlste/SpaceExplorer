2efdbc392df0b37d31a386cabb23f462934c1501
stephani.schielke <stephani.schielke@gmail.com>
2011-03-30 12:56:21 -0600

    Changed title bar

--- sexplorer.cpp
+++ sexplorer.cpp
@@ -20,7 +20,7 @@ void main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(320,320);
-	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+	glutCreateWindow("Space Explorer");
 
 	// register callbacks
 	glutDisplayFunc(renderScene);
