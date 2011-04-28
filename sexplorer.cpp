064fa89385eb283d9f89b330525a3b7fa9ce791d
ecarlste <ecarlste@gmail.com>
2011-04-28 08:41:45 -0600

    Changed the name of the main file to be a bit more politically correct

--- sexplorer.cpp
+++ sexplorer.cpp
@@ -140,39 +140,7 @@ void renderScene(void) {
     glRotatef(rotation_y,0.0,1.0,0.0);
     glRotatef(rotation_z,0.0,0.0,1.0);
 	
-	glBindTexture(GL_TEXTURE_2D, object.id_texture); // We set the active texture 
-
-    glBegin(GL_TRIANGLES); // glBegin and glEnd delimit the vertices that define a primitive (in our case triangles)
-    for (l_index=0;l_index<object.polygons_qty;l_index++)
-    {
-        //----------------- FIRST VERTEX -----------------
-        // Texture coordinates of the first vertex
-        glTexCoord2f( object.mapcoord[ object.polygon[l_index].a ].u,
-                      object.mapcoord[ object.polygon[l_index].a ].v);
-        // Coordinates of the first vertex
-        glVertex3f( object.vertex[ object.polygon[l_index].a ].x,
-                    object.vertex[ object.polygon[l_index].a ].y,
-                    object.vertex[ object.polygon[l_index].a ].z); //Vertex definition
-
-        //----------------- SECOND VERTEX -----------------
-        // Texture coordinates of the second vertex
-        glTexCoord2f( object.mapcoord[ object.polygon[l_index].b ].u,
-                      object.mapcoord[ object.polygon[l_index].b ].v);
-        // Coordinates of the second vertex
-        glVertex3f( object.vertex[ object.polygon[l_index].b ].x,
-                    object.vertex[ object.polygon[l_index].b ].y,
-                    object.vertex[ object.polygon[l_index].b ].z);
-        
-        //----------------- THIRD VERTEX -----------------
-        // Texture coordinates of the third vertex
-        glTexCoord2f( object.mapcoord[ object.polygon[l_index].c ].u,
-                      object.mapcoord[ object.polygon[l_index].c ].v);
-        // Coordinates of the Third vertex
-        glVertex3f( object.vertex[ object.polygon[l_index].c ].x,
-                    object.vertex[ object.polygon[l_index].c ].y,
-                    object.vertex[ object.polygon[l_index].c ].z);
-    }
-    glEnd();
+	renderShip();
 
 	glFlush();
     glutSwapBuffers();
